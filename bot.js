81e12632b4c3ae3de5da6b7d8bef0b4270542504
hanahaneull <48522963+hanahaneull@users.noreply.github.com>
2019-05-20 19:16:12 +0700

    Added random words

--- bot.js
+++ bot.js
@@ -16,14 +16,15 @@ try {
     if (command === "spam") {
       var count = 1; // Number of messages sent (modified by sendSpamMessage)
       var maxMessages = 10000; // Change based on how many messages you want sent
+      let pesan = ["Breakfast procuring nay end happiness allowance assurance frankness.", "Met simplicity nor difficulty unreserved who.", "Entreaties mr conviction dissimilar me astonished estimating cultivated. On no applauded exquisite my additions.", "Pronounce add boy estimable nay suspected. You sudden nay elinor thirty esteem temper. Quiet leave shy you gay off asked large style.", "Sex and neglected principle ask rapturous consulted.", "Object remark lively all did feebly excuse our wooded.", "Old her object chatty regard vulgar missed. Speaking throwing breeding betrayed children my to.", "Me marianne no he horrible produced ye.", "Sufficient unpleasing an insensible motionless if introduced ye. Now give nor both come near many late.", "However venture pursuit he am mr cordial.", "Forming musical am hearing studied be luckily.", "Ourselves for determine attending how led gentleman sincerity.", "Valley afford uneasy joy she thrown though bed set.", "In me forming general prudent on country carried.", "Behaved an or suppose justice.", "Seemed whence how son rather easily and change missed.", "Off apartments invitation are unpleasant solicitude fat motionless interested.", "Hardly suffer wisdom wishes valley as an. As friendship advantages resolution it alteration stimulated he or increasing.", "Attachment apartments in delightful by motionless it no.", "And now she burst sir learn total.", "Hearing hearted shewing own ask.", "Solicitude uncommonly use her motionless not collecting age.", "The properly servants required mistaken outlived bed and.", "Remainder admitting neglected is he belonging to perpetual objection up.", "Has widen too you decay begin which asked equal any.", "Wise busy past both park when an ye no."];
 
       function sendSpamMessage() {
         // You could modify this to send a random string from an array (ex. a quote), create a
         // random sentence by pulling words from a dictionary file, or to just send a random
         // arrangement of characters and integers. Doing something like this may help prevent
         // future moderation bots from detecting that you sent a spam message.
-        message.channel.send("asade kentot, sama lo semua ea ea ea bwang" + count);
-		console.log("Message = " + count);
+        message.channel.send(pesan[~~(Math.random() * pesan.length)]);
+		    console.log("Message = " + count);
 		
 		if (count < maxMessages) {
           // If you don't care about whether the messages are deleted or not, like if you created a dedicated server
